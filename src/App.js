6f32206caa8744d3bb19b2b7bf874affcdb5bcfa
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-09-25 23:43:41 +0600

    react-post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadComments></LoadComments>
     </div>
   );
+}
+
+function LoadComments(){
+  const [comments, setComments] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setComments(data))
+  }, [])
+
+  return (
+    <div>
+      <h2>Load Comments</h2>
+      {
+        comments.map(comment => <Comment title={comment.title} body={comment.body}></Comment>)
+      }
+    </div>
+  )
+}
+
+function Comment(props){
+  const commentStyle = {
+    backgroundColor: '',
+    backgroundColor: '#61dafb',
+    margin: '20px',
+    border: '3px solid blue',
+    borderRadius: '10px'
+  }
+  return (
+    <div style={commentStyle}>
+      <h3>{props.title}</h3>
+      <p>{props.body}</p>
+    </div>
+  )
 }
 
 export default App;
